5f1ae41f29024d362643f30d988a766f32d0caec
Alejandro Gallo <aamsgallo@gmail.com>
2016-09-12 15:57:47 +0200

    Automatic update

--- search/all_0.js
+++ search/all_0.js
@@ -3,6 +3,7 @@ var searchData=
   ['a',['a',['../structBasis.html#aa1859f20d91a29f29752197c97144102',1,'Basis']]],
   ['a_5flabel',['a_label',['../structBasis.html#a57861ee48a56aef16847900834e8d479',1,'Basis']]],
   ['atom',['Atom',['../structAtom.html',1,'']]],
+  ['atomcollection',['AtomCollection',['../structAtomCollection.html',1,'']]],
   ['atomic_5fnumber',['atomic_number',['../structAtomInfo.html#a8b1cbc353b0a176c2dab119e2b647d0a',1,'AtomInfo']]],
   ['atomic_5fradius',['atomic_radius',['../structAtomInfo.html#a6cfe07e180bcab615c20080d53ded66a',1,'AtomInfo']]],
   ['atominfo',['AtomInfo',['../structAtomInfo.html',1,'']]]
--- search/classes_0.js
+++ search/classes_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['atom',['Atom',['../structAtom.html',1,'']]],
+  ['atomcollection',['AtomCollection',['../structAtomCollection.html',1,'']]],
   ['atominfo',['AtomInfo',['../structAtomInfo.html',1,'']]]
 ];
